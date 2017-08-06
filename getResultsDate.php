254178eb1f7c4ccf5cf0edebf5837d59644a6ba3
Hans Krebs <hanskrebs@Hanss-MacBook-Air.local>
2017-08-06 14:55:08 +0200

    .

--- getResultsDate.php
+++ getResultsDate.php
@@ -3,5 +3,5 @@ require_once ('FileOperations.php');
 
 if (file_exists(FileNames::imdbQueryMoviesWithRatingsName())) {
     $videos = unserialize(file_get_contents( FileNames::imdbQueryMoviesWithRatingsName()));
-    echo date ("d. F Y, H:i:s", filemtime(FileNames::imdbQueryMoviesWithRatingsName()) . ", (" . count($videos) . " Videos)");
+    echo date ("d. F Y, H:i:s", filemtime(FileNames::imdbQueryMoviesWithRatingsName())) . ", Total Movies with Ratings: " . count($videos);
 }
