3b54524e2498799d7391811a1182b029223f529f
Hans Krebs <hanskrebs@Hanss-MacBook-Air.local>
2017-08-06 17:37:16 +0200

    SQL funktioniert

--- testSQLCon.php
+++ testSQLCon.php
@@ -1,4 +1,5 @@
 <?php
+require_once ('commons.php');
 
 /*
  * Root User: admin8Gu6jMU
@@ -16,9 +17,26 @@ define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
 
 $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
-$dbh = new PDO($dsn, DB_USER, DB_PASS);
+$pdo = new PDO($dsn, DB_USER, DB_PASS);
 
-$sql = "SELECT * FROM testTable";
-foreach ($pdo->query($sql) as $row) {
-    echo $row['key'] . " " . $row['value'] . "<br>";
+$sql = "SELECT * FROM users";
+$rows = $pdo->query($sql);
+echo "errorCode " .  $pdo->errorCode() . "<br>";
+echo "errorInfo " .  $pdo->errorInfo() . "<br>";
+
+echo "DB Rows <br>";
+
+foreach ($rows as $row) {
+    echo $row['datum'] . " , " . $row['firstname'] . " " . $row['lastname'] . "<br>";
 }
+
+
+// Die Tabelle imdbprime.users hat die spalen: id, datum, firstname, lastname
+// spalte id ist primary key und darf deshalb A_I, also auto_increment aktiv haben
+// deshalb wird dieser Wert von der DB automatisch gesetzt (darf nicht vom user gesetzt werden?)
+$statement = $pdo->prepare("INSERT INTO " . DB_NAME . ".users (datum, firstname, lastname) VALUES (?, ?, ?)");
+$statement->execute(array(nowAsString(), 'Hans', 'Lena'));
+echo "errorCode " .  $pdo->errorCode() . "<br>";
+echo "errorInfo " .  $pdo->errorInfo() . "<br>";
+$neue_id = $pdo->lastInsertId();
+echo "Neuer Nutzer mit id $neue_id angelegt";
