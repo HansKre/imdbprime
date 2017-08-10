106ba1f5c1494f3e7bd975286d5d1aac416fefcf
Hans Krebs <hanskrebs@Hanss-MacBook-Air.local>
2017-08-10 13:01:19 +0200

    .

--- forCron.php
+++ forCron.php
@@ -6,8 +6,23 @@ if (file_exists(FileNames::imdbQueryMoviesWithRatingsName())) {
     if (count($videos) < 100) {
         http_response_code(400);
     } else {
-        http_response_code(200);
+        http_response_code(400);
     }
 } else {
     http_response_code(400);
 }
+
+/*
+ * Use following code to query on external webserver
+
+<?php
+file_get_contents('http://imdbprime-snah.rhcloud.com/forCron.php');
+
+if ($http_response_header) {
+    if (substr($http_response_header[0], 9, 3) == 200) {
+        http_response_code(200);
+    } else {
+        http_response_code(400);
+    }
+}
+*/
