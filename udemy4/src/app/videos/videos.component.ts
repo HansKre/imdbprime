20677ccce0428503c3a0c6f2c3f7c49e06b5b273
Hans Krebs <hanskrebs@Hanss-MacBook-Air.local>
2017-08-13 12:44:53 +0200

    .

--- udemy4/src/app/videos/videos.component.ts
+++ udemy4/src/app/videos/videos.component.ts
@@ -16,15 +16,39 @@ export class VideosComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.preLoadMoviesFromLocalStorage();
         this.registerForWebRequest();
+    }
+
+    preLoadMoviesFromLocalStorage() {
+        if (localStorage) {
+            if (localStorage.movies) {
+                this.movies = JSON.parse(localStorage.movies);
+            }
+        }
+    }
+
+    storeMoviesToLocalStorage() {
+        if (localStorage) {
+            localStorage.movies = JSON.stringify(this.movies);
+        }
     }
 
     registerForWebRequest() {
         this.webService.getPromise().then(function (movies) {
             this.movies = movies as any as Movie[];
+            this.storeMoviesToLocalStorage();
         }.bind(this), function (error) {
             alert("Movies could not be retrieved from the web service.");
             console.log(error);
         }.bind(this));
+    }
+
+    useLocalStorage() {
+        if (!localStorage.pageLoadCount) {
+            localStorage.pageLoadCount = 0;
+        }
+        localStorage.pageLoadCount = parseInt(localStorage.pageLoadCount) + 1;
+        alert(localStorage.pageLoadCount);
     }
 }
